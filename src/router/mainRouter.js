7831cdefe89d61568e6632c17e2c1f0e03df002e
steve_tsao <steve_tsao@neithnet.con>
2022-02-21 10:13:47 +0800

    init

---
+++ src/router/mainRouter.js
@@ -0,0 +1,22 @@
+import Home from '../views/Home.vue';
+
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/newpage',
+    name: 'NewPage',
+    component: () => import(/* webpackChunkName: "newpage" */ '../views/NewPage.vue'),
+  },
+];
