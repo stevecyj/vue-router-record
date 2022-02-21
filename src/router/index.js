5dec9925c1e39b3b7121abbab032cd02e8646ccd
steve_tsao <steve_tsao@neithnet.con>
2022-02-21 17:03:35 +0800

    router settings

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,16 @@ const routes = [...mainRouter];
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  linkActiveClass: 'active',
+  scrollBehavior: (to, from, savedPosition) => {
+    console.log(to, from, savedPosition);
+    if (to.fullPath.match('newpage')) {
+      return { top: 0 };
+    }
+    return {
+      // top: 500,
+    };
+  },
 });
 
 export default router;
